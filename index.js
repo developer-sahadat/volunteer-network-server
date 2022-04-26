1ef1527fde6bca1e02ab8ed408f879d24d6a9bfd
developer-sahadat <mdridoysikder862@gmail.com>
2022-04-27 01:28:32 +0600

    get and push method added

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xjkn1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  console.log("mongoDB conected");
-  client.close();
-});
 
-async function run() {}
+async function run() {
+  try {
+    await client.connect();
+    const collection = client.db("volunteerNetwork").collection("event");
+    const orderCollection = client.db("volunteerNetwork").collection("order");
+
+    // all event get api
+    app.get("/event", async (req, res) => {
+      const query = {};
+      const cursor = collection.find(query);
+      const event = await cursor.toArray();
+      res.send(event);
+    });
+
+    // order details
+    app.post("/order", async (req, res) => {
+      const user = req.body;
+      const result = await orderCollection.insertOne(user);
+      res.send(result);
+    });
+  } finally {
+  }
+}
 
 run().catch(console.dir);
 
