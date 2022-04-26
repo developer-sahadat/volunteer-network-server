e1f5e994a62c3a8a225a37d5c6faec788b344074
developer-sahadat <mdridoysikder862@gmail.com>
2022-04-26 23:15:58 +0600

    mongo DB concected

--- index.js
+++ index.js
@@ -5,6 +5,24 @@ const app = express();
 
 app.use(cors());
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xjkn1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log("mongoDB conected");
+  client.close();
+});
+
+async function run() {}
+
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("All Ok");
 });
@@ -12,3 +30,6 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(port);
 });
+
+//volunteer-network
+//ws920q7Gr3CDGo1V
